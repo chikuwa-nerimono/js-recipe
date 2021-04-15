529f628c6865297c0b0a7a6e04829d7a88beb06b
chikuwa <chisa_015@outlook.jp>
2021-04-15 23:36:21 +0900

    square and circle ex1

--- semi/week1/main.js
+++ semi/week1/main.js
@@ -1,12 +1,5 @@
-const display = document.getElementById("display")
-const plusButton = document.getElementById("plus-button")
+const figure = document.getElementById("figure")
 
-let count = 0
-
-// ボタン要素のonclickハンドラに関数を代入する
-plusButton.onclick = function() {
-  // count を更新
-  count += 1
-  // count を表示
-  display.textContent = count
+figure.onmousemove = function() {
+  figure.classList.toggle("rounded")
 }
