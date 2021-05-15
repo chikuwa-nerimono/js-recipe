3283f2fe1248ea011872c17445393ec164b03efd
chikuwa <chisa_015@outlook.jp>
2021-05-15 14:46:41 +0900

    vue-counter

---
+++ semi/week4/vue-apl/main.js
@@ -0,0 +1,47 @@
+new Vue({
+  el: "#v-if",
+  data: {
+    seen: false,
+  },
+  methods: {
+    hyouji: function() {
+      this.seen = true
+    },
+  },
+})
+
+new Vue({
+  el: "#v-if-sign-in",
+  data: {
+    user: null,
+  },
+  methods: {
+    signIn() {
+      this.user = {
+        name: "ギーク太郎",
+      }
+    },
+    signOut() {
+      this.user = null
+    },
+  },
+})
+
+new Vue({
+  el: "#v-for",
+  data: {
+    animals: ["ふらみんご", "ごりら", "らいおん"],
+  },
+})
+
+new Vue({
+  el: "#v-for-push",
+  data: {
+    animals: ["ふらみんご", "ごりら", "らいおん"],
+  },
+  methods: {
+    addAnimal: function() {
+      this.animals.push("んらいおん")
+    },
+  },
+})
